cc8f0f509f18637b4bcb499b3e28c22da8a06ad4
Aditya Purandare <aditya.p1993@hotmail.com>
2016-01-06 01:27:23 -0600

    Updates Readme

--- create-server.js
+++ create-server.js
@@ -1,8 +1,14 @@
 var server = http.createServer(function(request, response){
     var filePath = false;
 
-    if(request.url == '/')
-    {
-        filePath = "app/index.html"
+    if(request.url == '/') {
+        filePath = "app/index.html";
+    } else {
+        filePath = "app" + request.url;
     }
+
+    var absPath = "./" + filePath;
+    serverWorking(response, absPath);
 });
+
+server.listen(process.env.PORT || 8888);
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/*
 var http = require("http");
 var fs= require("fs");
 var path = require("path");
@@ -14,12 +13,13 @@ function sendPage(response, filePath, fileContents){
     response.writeHead(200, {"Content-type": mime.lookup(path.basename(filePath))});
     reponse.end(fileContents);
 }
-*/
 
-var express = require('express');
+
+/*var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + "/dist"));
 
 var port = process.env.PORT || 3000;
 app.listen(port);
+*/
