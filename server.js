1096705e0d114525642f6812b3a3cea1dd1d09e2
Aditya Purandare <aditya.p1993@hotmail.com>
2016-01-06 01:31:28 -0600

    Add Server

--- server.js
+++ server.js
@@ -1,25 +1,46 @@
 var http = require("http");
-var fs= require("fs");
+var fs = require("fs");
 var path = require("path");
 var mime = require("mime");
 
-function send404(response){
-    response.writeHead(404, {"Content-type":"text/plain"});
-    response.write("Oops 404 Error: Resource not found");
+function send404(response) {
+    response.writeHead(404, {"Content-type" : "text/plain"});
+    response.write("Error 404: resource not found");
     response.end();
 }
 
-function sendPage(response, filePath, fileContents){
-    response.writeHead(200, {"Content-type": mime.lookup(path.basename(filePath))});
-    reponse.end(fileContents);
+function sendPage(response, filePath, fileContents) {
+    response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+    response.end(fileContents);
 }
 
+function serverWorking(response, absPath) {
+    fs.exists(absPath, function(exists) {
+        if (exists) {
+            fs.readFile(absPath, function(err, data) {
+                if (err) {
+                    send404(response)
+                } else {
+                    sendPage(response, absPath, data);
+                }
+            });
+        } else {
+            send404(response);
+        }
+    });
+}
+        
+var server = http.createServer(function(request, response) {
+    var filePath = false;
 
-/*var express = require('express');
-var app = express();
+    if (request.url == "/") {
+        filePath = "app/index.html";
+    } else {
+        filePath = "app" + request.url;
+    }
 
-app.use(express.static(__dirname + "/dist"));
+    var absPath = "./" + filePath;
+    serverWorking(response, absPath);
+});
 
-var port = process.env.PORT || 3000;
-app.listen(port);
-*/
+var port_number = server.listen(process.env.PORT || 3000);
