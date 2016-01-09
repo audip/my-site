144ce5fa90b632585467cfce7ff956638e8ae695
Aditya Purandare <aditya.p1993@hotmail.com>
2016-01-08 20:29:16 -0600

    Add post data to express

--- app/js/contact_me.js
+++ app/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail",
+                url: "././index.html",
                 type: "POST",
                 data: {
                     name: name,
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ function sendPage(response, filePath, fileContents) {
 }
 
 app.use(express.static('./app'));
+app.use(bodyParser.text({ type: 'text/html' }));
 
 app.get('/', function(req, res){
     var filePath = false;
@@ -41,8 +42,38 @@ app.get('/', function(req, res){
     });
 });
 
-app.post('/mail', function(req, res){
-    console.log("Mail sent");
+app.post('/', function(req, res){
+
+    console.log("I am here");
+
+    var nodemailer = require('nodemailer');
+    var bodyParser = require('body-parser');
+
+    //console.log(req.body.username);
+
+    // create reusable transporter object using the default SMTP transport
+    var transporter = nodemailer.createTransport('smtps://aditya.p1993@hotmail.com:GoogleEarth1993@smtp.hotmail.com');
+
+    // NB! No need to recreate the transporter object. You can use
+    // the same transporter object for all e-mails
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
+        to: 'Aditya Purandare <hello@adityapurandare.ml>', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world ✔', // plaintext body
+        html: '<b>Hello world ✔</b>' // html body
+    };
+
+    // send mail with defined transport object
+    /*transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+
+    });*/
 });
 
 app.listen(process.env.PORT || 3000);
